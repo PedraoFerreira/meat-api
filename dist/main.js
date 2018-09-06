7ba2764ea4cfc90cc19cfb297c5158333b1803b3
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-06 20:12:27 -0300

    aula 19

--- dist/main.js
+++ dist/main.js
@@ -1,21 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const restify = require("restify");
-const server = restify.createServer({
-    name: 'meat-api',
-    version: '1.0.0'
-});
-server.use(restify.plugins.queryParser());
-server.get('/info', (req, resp, next) => {
-    resp.json({
-        browser: req.userAgent(),
-        method: req.method,
-        url: req.href(),
-        path: req.path(),
-        query: req.query
-    });
-    return next();
-});
-server.listen(3000, () => {
-    console.log('API is running on http://localhost:3000');
+const server_1 = require("./server/server");
+const users_router_1 = require("./users/users.router");
+const server = new server_1.Server();
+server.bootstrap([users_router_1.usersRouter]).then(server => {
+    console.log('Server is listening on:', server.application.address());
+}).catch(error => {
+    console.log('Server failed to start');
+    console.error(error);
+    process.exit(1);
 });
